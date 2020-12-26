0c1caec259056d69071df5c918e8b693ad3eebd3
Chi Yeung <yeun0007@umn.edu>
2020-12-26 15:31:50 -0600

    enum mock

--- MockKEx/app/src/test/java/com/ctyeung/mockkex/SingletonTest.kt
+++ MockKEx/app/src/test/java/com/ctyeung/mockkex/SingletonTest.kt
@@ -1,5 +1,6 @@
 package com.ctyeung.mockkex
 
+import io.mockk.every
 import io.mockk.mockkObject
 import io.mockk.unmockkAll
 import io.mockk.unmockkObject
@@ -12,6 +13,12 @@ object Singleton {
     }
 }
 
+enum class StateEnum(val index:Int) {
+    MA(1),
+    MD(2),
+    MN(3);
+}
+
 class testSingleton {
 
     @Test
@@ -19,6 +26,14 @@ class testSingleton {
         mockkObject(Singleton)
         val sum = Singleton.add(2,2)
         assertEquals(4, sum)
+        unmockkAll()
+    }
+
+    @Test
+    fun testEnumMockObj() {
+        mockkObject(StateEnum.MA)
+        every { StateEnum.MA.index } returns 5
+        assertEquals(5, StateEnum.MA.index)
         unmockkAll()
     }
 }
